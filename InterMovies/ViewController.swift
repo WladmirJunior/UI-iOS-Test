54abce652dd0f7470f4c208583676f649c68520f
Wladmir Edmar Silva Junior <wladmir.silva@bancointer.com.br>
2021-08-11 09:40:19 -0300

    Initial Commit

---
+++ InterMovies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InterMovies
+//
+//  Created by Wladmir Edmar Silva Junior on 11/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
